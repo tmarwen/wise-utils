47bcf52d1e076477c8356398598153904fe375d0
tmarwen <marwen.trabelsi.insat@gmail.com>
2014-06-18 13:26:10 +0100

    [FIX][IMPROVEMENT] | Fix some minor issues and add an operation enum

--- standalone/src/main/java/org/wisebrains/utils/file/FileMinifier.java
+++ standalone/src/main/java/org/wisebrains/utils/file/FileMinifier.java
@@ -1,5 +1,8 @@
 package org.wisebrains.utils.file;
 
+import org.apache.commons.io.FileUtils;
+import org.wisebrains.utils.operation.OperationType;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -12,6 +15,14 @@ import java.io.IOException;
  * Created by eXo Platform MEA on 26/05/14.
  *
  * @author <a href="mailto:mtrabelsi@exoplatform.com">Marwen Trabelsi</a>
+ *
+ * This utility artifact can be used either to shrink a file by removing repeatable lines or to
+ * extract some lines to another file.
+ * The counter is launched once the search key is found. It limits to either a stop token if specified
+ * Or when reaching some range of lines when the flag "withLinesCount" is specified.
+ *
+ * Use below commands
+ *
  */
 public class FileMinifier
 {
@@ -20,6 +31,8 @@ public class FileMinifier
   private String searchKey;
   private int linesToCount;
   private String stopKey;
+  private static final String REMOVE = "remove";
+  private static final String EXTRACT = "extract";
 
   public FileMinifier(String fileFromPath, String fileToPath, String searchKey, int linesToCount)
   {
@@ -56,32 +69,69 @@ public class FileMinifier
 
   public static void main(String[] args)
   {
-    FileMinifier fileMinifier;
+    args = new String[5];
+    args[0] = "remove";
+    args[1] = "withtoutLinesCount";
+    args[2] = "/home/exo/Work/working/customers-issues/SEE/SEE-20/tdump/dump";
+    args[3] = "by \"TP-Processor39\"";
+    args[4] = "at java.lang.Thread.run";
 
-    if (args.length > 4)
+    FileMinifier fileMinifier;
+    OperationType operation = OperationType.valueOf(args[0].toUpperCase());
+
+    switch (operation)
     {
-      if (args[0].equals("withLinesCount"))
+      case REMOVE:
       {
-        fileMinifier = new FileMinifier(args[1], args[2], args[3], Integer.parseInt(args[4]));
-        fileMinifier.removeFromFileWithLineCount();
+        if (args.length > 5)
+        {
+          if (args[1].equals("withLinesCount"))
+          {
+            fileMinifier = new FileMinifier(args[2], args[3], args[4], Integer.parseInt(args[5]));
+            fileMinifier.removeFromFileWithLineCount();
+          } else
+          {
+            fileMinifier = new FileMinifier(args[2], args[3], args[4], args[5]);
+            fileMinifier.removeFromFileWithKeyStop();
+          }
+        } else
+        {
+          if (args[1].equals("withLinesCount"))
+          {
+            fileMinifier = new FileMinifier(args[2], args[3], Integer.parseInt(args[4]));
+            fileMinifier.removeFromFileWithLineCount();
+          } else
+          {
+            fileMinifier = new FileMinifier(args[2], args[3], args[4]);
+            fileMinifier.removeFromFileWithKeyStop();
+          }
+        }
       }
-      else
+      case EXTRACT:
       {
-        fileMinifier = new FileMinifier(args[1], args[2], args[3], args[4]);
-        fileMinifier.removeFromFileWithKeyStop();
-      }
-    }
-    else
-    {
-      if (args[0].equals("withLinesCount"))
-      {
-        fileMinifier = new FileMinifier(args[1], args[2], Integer.parseInt(args[3]));
-        fileMinifier.removeFromFileWithLineCount();
-      }
-      else
-      {
-        fileMinifier = new FileMinifier(args[1], args[2], args[3]);
-        fileMinifier.removeFromFileWithKeyStop();
+        if (args.length > 5)
+        {
+          if (args[1].equals("withLinesCount"))
+          {
+            fileMinifier = new FileMinifier(args[2], args[3], args[4], Integer.parseInt(args[5]));
+            fileMinifier.extractToFileWithLineCount();
+          } else
+          {
+            fileMinifier = new FileMinifier(args[2], args[3], args[4], args[5]);
+            fileMinifier.removeFromFileWithKeyStop();
+          }
+        } else
+        {
+          if (args[1].equals("withLinesCount"))
+          {
+            fileMinifier = new FileMinifier(args[2], args[3].concat("-EXTRACTED"), args[3], Integer.parseInt(args[4]));
+            fileMinifier.removeFromFileWithLineCount();
+          } else
+          {
+            fileMinifier = new FileMinifier(args[2], args[2].concat("-EXTRACTED"), args[3], args[4]);
+            fileMinifier.extractToFileWithKeyStop();
+          }
+        }
       }
     }
   }
@@ -91,7 +141,7 @@ public class FileMinifier
     File sourceFile = new File(fileFromPath);
     File destinationFile = new File(fileToPath);
     BufferedReader br = null;
-    BufferedWriter bw =  null;
+    BufferedWriter bw = null;
 
     try
     {
@@ -106,8 +156,7 @@ public class FileMinifier
           {
             br.readLine();
           }
-        }
-        else
+        } else
         {
           bw.write(currentLine.concat("\n"));
         }
@@ -118,8 +167,7 @@ public class FileMinifier
     } catch (IOException e)
     {
       e.printStackTrace();
-    }
-    finally
+    } finally
     {
       try
       {
@@ -132,12 +180,51 @@ public class FileMinifier
     }
   }
 
+  public void extractToFileWithLineCount()
+  {
+    File sourceFile = new File(fileFromPath);
+    File destinationFile = new File(fileToPath);
+    BufferedReader br = null;
+
+    try
+    {
+      br = new BufferedReader(new FileReader(sourceFile));
+      String currentLine;
+      while ((currentLine = br.readLine()) != null)
+      {
+        if (currentLine.contains(searchKey))
+        {
+          for (int i = 0; i < linesToCount; i++)
+          {
+            FileUtils.writeStringToFile(destinationFile, currentLine.concat("\n"), "UTF-8", true);
+            currentLine = br.readLine();
+          }
+        }
+      }
+    } catch (FileNotFoundException e)
+    {
+      e.printStackTrace();
+    } catch (IOException e)
+    {
+      e.printStackTrace();
+    } finally
+    {
+      try
+      {
+        br.close();
+      } catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+    }
+  }
+
   public void removeFromFileWithKeyStop()
   {
     File sourceFile = new File(fileFromPath);
     File destinationFile = new File(fileToPath);
     BufferedReader br = null;
-    BufferedWriter bw =  null;
+    BufferedWriter bw = null;
 
     try
     {
@@ -153,8 +240,7 @@ public class FileMinifier
             currentLine = br.readLine();
           }
           while (!(currentLine.contains(stopKey)) && (currentLine != null));
-        }
-        else
+        } else
         {
           bw.write(currentLine.concat("\n"));
         }
@@ -165,13 +251,59 @@ public class FileMinifier
     } catch (IOException e)
     {
       e.printStackTrace();
-    }
-    finally
+    } finally
     {
       try
       {
         br.close();
         bw.close();
+      } catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void extractToFileWithKeyStop()
+  {
+    File sourceFile = new File(fileFromPath);
+    File destinationFile = new File(fileToPath);
+    BufferedReader br = null;
+    boolean quitIteration;
+
+    try
+    {
+      br = new BufferedReader(new FileReader(sourceFile));
+      String currentLine;
+      while ((currentLine = br.readLine()) != null)
+      {
+        if ((currentLine.contains(searchKey)) && (currentLine != null))
+        {
+          quitIteration = false;
+          do
+          {
+            FileUtils.writeStringToFile(destinationFile, currentLine.concat("\n"), "UTF-8", true);
+            currentLine = br.readLine();
+            if (currentLine.contains(stopKey) && (currentLine != null))
+            {
+              FileUtils.writeStringToFile(destinationFile, currentLine.concat("\n"), "UTF-8", true);
+              quitIteration = true;
+            }
+          }
+          while (!quitIteration);
+        }
+      }
+    } catch (FileNotFoundException e)
+    {
+      e.printStackTrace();
+    } catch (IOException e)
+    {
+      e.printStackTrace();
+    } finally
+    {
+      try
+      {
+        br.close();
       } catch (IOException e)
       {
         e.printStackTrace();
---
+++ standalone/src/main/java/org/wisebrains/utils/operation/OperationType.java
@@ -0,0 +1,11 @@
+package org.wisebrains.utils.operation;
+
+/**
+ * Created by eXo Platform MEA on 27/05/14.
+ *
+ * @author <a href="mailto:mtrabelsi@exoplatform.com">Marwen Trabelsi</a>
+ */
+public enum OperationType
+{
+  REMOVE, EXTRACT;
+}
